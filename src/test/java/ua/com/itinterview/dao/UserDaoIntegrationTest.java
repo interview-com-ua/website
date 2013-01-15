c1fe40f226ab10fb333b063c3d2fc8e99832e6f4
vkuchyn <kuchin.victor@gmail.com>
2013-01-15 21:02:13 +0200

    added test getUserByUserName for non existing user name

--- src/test/java/ua/com/itinterview/dao/UserDaoIntegrationTest.java
+++ src/test/java/ua/com/itinterview/dao/UserDaoIntegrationTest.java
@@ -2,6 +2,8 @@ package ua.com.itinterview.dao;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,6 +22,14 @@ public class UserDaoIntegrationTest extends
 		userDao.save(user);
 		UserEntity actual = userDao.getUserByUserName("userName");
 		assertEquals("userName", actual.getUserName());
+	}
+
+	@Test(expected = EntityNotFoundException.class)
+	public void testGetUserByUserNameWhenNotExistsAndRecesiveException() {
+		UserEntity user = new UserEntity();
+		user.setUserName("userName");
+		userDao.save(user);
+		userDao.getUserByUserName("userFakeName");
 	}
 
 	@Override
