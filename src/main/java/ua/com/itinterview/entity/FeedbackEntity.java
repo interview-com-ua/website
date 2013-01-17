fe9393514dca0a6e15f0ec36d1e25adf963c17e7
Timofey <timofiy.bilyi@gmail.com>
2013-01-17 17:50:34 +0200

    create class FeedbackEntity

---
+++ src/main/java/ua/com/itinterview/entity/FeedbackEntity.java
@@ -0,0 +1,82 @@
+package ua.com.itinterview.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "feedbacks")
+@SequenceGenerator(name = "sequence", sequenceName = "feedback_id", allocationSize = 1)
+public class FeedbackEntity extends EntityWithId {
+    // text
+    // timestamp
+    private String feedbackText;
+    private Timestamp createTime;
+    private boolean checked;
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result
+		+ ((createTime == null) ? 0 : createTime.hashCode());
+	result = prime * result
+		+ ((feedbackText == null) ? 0 : feedbackText.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	FeedbackEntity other = (FeedbackEntity) obj;
+	if (createTime == null) {
+	    if (other.createTime != null)
+		return false;
+	} else if (!createTime.equals(other.createTime))
+	    return false;
+	if (feedbackText == null) {
+	    if (other.feedbackText != null)
+		return false;
+	} else if (!feedbackText.equals(other.feedbackText))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "FeedbackEntity [feedbackText=" + feedbackText + ", createTime="
+		+ createTime + "]";
+    }
+
+    public String getFeedbackText() {
+	return feedbackText;
+    }
+
+    public void setFeedbackText(String feedbackText) {
+	this.feedbackText = feedbackText;
+    }
+
+    public Timestamp getCreateTime() {
+	return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+	this.createTime = createTime;
+    }
+
+    public boolean isChecked() {
+	return checked;
+    }
+
+    public void setChecked(boolean checked) {
+	this.checked = checked;
+    }
+
+}
--- src/test/java/ua/com/itinterview/dao/UserDaoIntegrationTest.java
+++ src/test/java/ua/com/itinterview/dao/UserDaoIntegrationTest.java
@@ -10,40 +10,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ua.com.itinterview.entity.UserEntity;
 
 public class UserDaoIntegrationTest extends
-		BaseEntityWithIdDaoIntegrationTest<UserEntity> {
+	BaseEntityWithIdDaoIntegrationTest<UserEntity> {
 
-	@Autowired
-	private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-	@Test
-	public void testGetUserByUserName() {
-		UserEntity user = new UserEntity();
-		user.setUserName("userName");
-		userDao.save(user);
-		UserEntity actual = userDao.getUserByUserName("userName");
-		assertEquals("userName", actual.getUserName());
-	}
+    @Test
+    public void testGetUserByUserName() {
+	UserEntity user = new UserEntity();
+	user.setUserName("userName");
+	userDao.save(user);
+	UserEntity actual = userDao.getUserByUserName("userName");
+	assertEquals("userName", actual.getUserName());
+    }
 
-	@Test(expected = EntityNotFoundException.class)
-	public void testGetUserByUserNameWhenNotExistsAndRecesiveException() {
-		UserEntity user = new UserEntity();
-		user.setUserName("userName");
-		userDao.save(user);
-		userDao.getUserByUserName("userFakeName");
-	}
+    @Test(expected = EntityNotFoundException.class)
+    public void testGetUserByUserNameWhenNotExistsAndRecesiveException() {
+	UserEntity user = new UserEntity();
+	user.setUserName("userName");
+	userDao.save(user);
+	userDao.getUserByUserName("userFakeName");
+    }
 
-	@Override
-	protected UserEntity createEntity() {
-		UserEntity user = new UserEntity();
-		user.setEmail("email@gmail.com");
-		user.setUserName("userName");
-		user.setPassword("password");
-		return user;
-	}
+    @Override
+    protected UserEntity createEntity() {
+	UserEntity user = new UserEntity();
+	user.setEmail("email@gmail.com");
+	user.setUserName("userName");
+	user.setPassword("password");
+	return user;
+    }
 
-	@Override
-	protected EntityWithIdDao<UserEntity> getEntityWithIdDao() {
-		return userDao;
-	}
+    @Override
+    protected EntityWithIdDao<UserEntity> getEntityWithIdDao() {
+	return userDao;
+    }
 
 }
