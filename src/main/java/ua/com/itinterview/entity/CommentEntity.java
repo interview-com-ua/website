0a73166015626ca44d3a4eec20374989ad5c734a
pmorozov <petia.morozov@gmail.com>
2013-01-17 23:24:42 +0200

    reformatting the code using Java Conventions profile, no other changes

--- src/main/java/ua/com/itinterview/entity/CommentEntity.java
+++ src/main/java/ua/com/itinterview/entity/CommentEntity.java
@@ -9,119 +9,120 @@ import javax.persistence.Table;
 @SequenceGenerator(name = "sequence", sequenceName = "comments_id", allocationSize = 1)
 public class CommentEntity extends EntityWithId {
 
-	private int questionId; //TODO: a reference to QuestionEntity might be needed here, appropriately annotated.
-	private int rate;
-	private String authorName;
-	private String email;
-	private String commentText;
-	private String userpicUrl;
+    private int questionId; // TODO: a reference to QuestionEntity might be
+			    // needed here, appropriately annotated.
+    private int rate;
+    private String authorName;
+    private String email;
+    private String commentText;
+    private String userpicUrl;
 
-	public int getQuestionId() {
-		return questionId;
-	}
+    public int getQuestionId() {
+	return questionId;
+    }
 
-	public void setQuestionId(int questionId) {
-		this.questionId = questionId;
-	}
+    public void setQuestionId(int questionId) {
+	this.questionId = questionId;
+    }
 
-	public int getRate() {
-		return rate;
-	}
+    public int getRate() {
+	return rate;
+    }
 
-	public void setRate(int rate) {
-		this.rate = rate;
-	}
+    public void setRate(int rate) {
+	this.rate = rate;
+    }
 
-	public String getAuthorName() {
-		return authorName;
-	}
+    public String getAuthorName() {
+	return authorName;
+    }
 
-	public void setAuthorName(String authorName) {
-		this.authorName = authorName;
-	}
+    public void setAuthorName(String authorName) {
+	this.authorName = authorName;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+	return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+	this.email = email;
+    }
 
-	public String getCommentText() {
-		return commentText;
-	}
+    public String getCommentText() {
+	return commentText;
+    }
 
-	public void setCommentText(String commentText) {
-		this.commentText = commentText;
-	}
+    public void setCommentText(String commentText) {
+	this.commentText = commentText;
+    }
 
-	public String getUserpicUrl() {
-		return userpicUrl;
-	}
+    public String getUserpicUrl() {
+	return userpicUrl;
+    }
 
-	public void setUserpicUrl(String userpicUrl) {
-		this.userpicUrl = userpicUrl;
-	}
+    public void setUserpicUrl(String userpicUrl) {
+	this.userpicUrl = userpicUrl;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result
-				+ ((authorName == null) ? 0 : authorName.hashCode());
-		result = prime * result
-				+ ((commentText == null) ? 0 : commentText.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + questionId;
-		result = prime * result + rate;
-		result = prime * result
-				+ ((userpicUrl == null) ? 0 : userpicUrl.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime * result
+		+ ((authorName == null) ? 0 : authorName.hashCode());
+	result = prime * result
+		+ ((commentText == null) ? 0 : commentText.hashCode());
+	result = prime * result + ((email == null) ? 0 : email.hashCode());
+	result = prime * result + questionId;
+	result = prime * result + rate;
+	result = prime * result
+		+ ((userpicUrl == null) ? 0 : userpicUrl.hashCode());
+	return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CommentEntity other = (CommentEntity) obj;
-		if (authorName == null) {
-			if (other.authorName != null)
-				return false;
-		} else if (!authorName.equals(other.authorName))
-			return false;
-		if (commentText == null) {
-			if (other.commentText != null)
-				return false;
-		} else if (!commentText.equals(other.commentText))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (questionId != other.questionId)
-			return false;
-		if (rate != other.rate)
-			return false;
-		if (userpicUrl == null) {
-			if (other.userpicUrl != null)
-				return false;
-		} else if (!userpicUrl.equals(other.userpicUrl))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	CommentEntity other = (CommentEntity) obj;
+	if (authorName == null) {
+	    if (other.authorName != null)
+		return false;
+	} else if (!authorName.equals(other.authorName))
+	    return false;
+	if (commentText == null) {
+	    if (other.commentText != null)
+		return false;
+	} else if (!commentText.equals(other.commentText))
+	    return false;
+	if (email == null) {
+	    if (other.email != null)
+		return false;
+	} else if (!email.equals(other.email))
+	    return false;
+	if (questionId != other.questionId)
+	    return false;
+	if (rate != other.rate)
+	    return false;
+	if (userpicUrl == null) {
+	    if (other.userpicUrl != null)
+		return false;
+	} else if (!userpicUrl.equals(other.userpicUrl))
+	    return false;
+	return true;
+    }
 
-	@Override
-	public String toString() {
-		return "CommentEntity [questionId=" + questionId + ", rate=" + rate
-				+ ", authorName=" + authorName + ", email=" + email
-				+ ", commentText=" + commentText + ", userpicUrl=" + userpicUrl
-				+ "]";
-	}	
+    @Override
+    public String toString() {
+	return "CommentEntity [questionId=" + questionId + ", rate=" + rate
+		+ ", authorName=" + authorName + ", email=" + email
+		+ ", commentText=" + commentText + ", userpicUrl=" + userpicUrl
+		+ "]";
+    }
 
 }
