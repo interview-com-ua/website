02d4fd79e65a32c70b1d155e13f29d625ac23c3d
olgakostrub <lady.irony.ua@gmail.com>
2013-01-18 15:06:27 +0200

    issue15

--- src/test/java/ua/com/itinterview/dao/BaseDaoIntegrationTest.java
+++ src/test/java/ua/com/itinterview/dao/BaseDaoIntegrationTest.java
@@ -26,79 +26,83 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import ua.com.itinterview.entity.PositionEntity;
+import ua.com.itinterview.entity.TechnologyEntity;
 import ua.com.itinterview.entity.UserEntity;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:context.xml" })
 public abstract class BaseDaoIntegrationTest extends
-		AbstractTransactionalJUnit4SpringContextTests {
+	AbstractTransactionalJUnit4SpringContextTests {
 
-	@Autowired
-	private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-	@Autowired
-	private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-	private final List<Class<?>> entities;
+    private final List<Class<?>> entities;
 
-	private final static String CLEANUP_TABLE_SQL = "delete %s";
+    private final static String CLEANUP_TABLE_SQL = "delete %s";
 
-	public BaseDaoIntegrationTest() {
-		entities = new ArrayList<Class<?>>();
-		entities.add(UserEntity.class);
+    public BaseDaoIntegrationTest() {
+	entities = new ArrayList<Class<?>>();
+	entities.add(UserEntity.class);
+	entities.add(PositionEntity.class);
+	entities.add(TechnologyEntity.class);
+    }
+
+    @Before
+    public void setUp() throws IOException {
+	try {
+	    cleanUpDb();
+	} catch (RuntimeException ex) {
+	    // TODO create smart structure of insert before, clean up after
 	}
-
-	@Before
-	public void setUp() throws IOException {
-		try {
-			cleanUpDb();
-		} catch (RuntimeException ex) {
-			// TODO create smart structure of insert before, clean up after
+	final String sql = readDbUpdateScriptFromFile(new File(
+		"sql/itinterview_ddl_schema.sql"));
+	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+	jdbcTemplate.execute(new StatementCallback<Object>() {
+	    public Object doInStatement(Statement stmt) throws SQLException,
+		    DataAccessException {
+		String[] scripts = sql.split(";");
+		for (String script : scripts) {
+		    try {
+			stmt.execute(script);
+		    } catch (Exception e) {
+			// TODO create auto ddl script
+		    }
 		}
-		final String sql = readDbUpdateScriptFromFile(new File(
-				"sql/itinterview_ddl_schema.sql"));
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		jdbcTemplate.execute(new StatementCallback<Object>() {
-			public Object doInStatement(Statement stmt) throws SQLException,
-					DataAccessException {
-				String[] scripts = sql.split(";");
-				for (String script : scripts) {
-					try {
-						stmt.execute(script);
-					} catch (Exception e) {
-						// TODO create auto ddl script
-					}
-				}
-				return null;
-			}
-		});
-	}
+		return null;
+	    }
+	});
+    }
 
-	public String readDbUpdateScriptFromFile(final File file)
-			throws IOException {
-		StringBuffer buffer = new StringBuffer();
-		FileInputStream fis = new FileInputStream(file);
-		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-		String line = "";
-		while ((line = br.readLine()) != null) {
-			buffer.append(line);
-		}
-		br.close();
-		return buffer.toString();
+    public String readDbUpdateScriptFromFile(final File file)
+	    throws IOException {
+	StringBuffer buffer = new StringBuffer();
+	FileInputStream fis = new FileInputStream(file);
+	BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+	String line = "";
+	while ((line = br.readLine()) != null) {
+	    buffer.append(line);
 	}
+	br.close();
+	return buffer.toString();
+    }
 
-	@After
-	@Transactional
-	public void cleanUpDb() {
-		try {
-			Session session = sessionFactory.getCurrentSession();
-			for (Class<?> ent : entities) {
-				session.createQuery(
-						String.format(CLEANUP_TABLE_SQL, ent.getSimpleName()))
-						.executeUpdate();
-			}
-		} catch (Exception ex) {
-			throw new RuntimeException(ex);
-		}
+    @After
+    @Transactional
+    public void cleanUpDb() {
+	try {
+	    Session session = sessionFactory.getCurrentSession();
+	    for (Class<?> ent : entities) {
+		session.createQuery(
+			String.format(CLEANUP_TABLE_SQL, ent.getSimpleName()))
+			.executeUpdate();
+	    }
+	} catch (Exception ex) {
+	    throw new RuntimeException(ex);
 	}
+    }
 }
