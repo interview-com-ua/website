247784d240973ef04d2461a3ebd3af54634df5d3
vkuchyn <kuchin.victor@gmail.com>
2013-01-22 15:39:49 +0200

    Added default index controller

---
+++ src/main/java/ua/com/itinterview/web/IndexController.java
@@ -0,0 +1,16 @@
+package ua.com.itinterview.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView showIndexPage() {
+	ModelAndView view = new ModelAndView("index");
+	return view;
+    }
+}
