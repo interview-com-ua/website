940b43cb28ba9eee154c4e2a715bb4fe07a68427
MaximPedich <maximpedich@gmail.com>
2013-01-23 22:04:36 +0200

    website-12

--- src/main/java/ua/com/itinterview/dao/QuestionDao.java
+++ src/main/java/ua/com/itinterview/dao/QuestionDao.java
@@ -2,54 +2,35 @@ package ua.com.itinterview.dao;
 
 import static org.hibernate.criterion.Restrictions.eq;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import ua.com.itinterview.entity.InterviewEntity;
 import ua.com.itinterview.entity.QuestionEntity;
 import ua.com.itinterview.entity.UserEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class QuestionDao extends EntityWithIdDao<QuestionEntity> {
-
-//    @Autowired
-//    protected SessionFactory sessionFactory;
 
     @SuppressWarnings("unchecked")
     @Transactional
     public List<QuestionEntity> getQuestionsForUser(UserEntity user) {
-        Session session = sessionFactory.getCurrentSession();
-        Criteria criteria = session.createCriteria(QuestionEntity.class);
-        criteria.add(eq("user", user));
-        return criteria.list();
+	Session session = sessionFactory.getCurrentSession();
+	Criteria criteria = session.createCriteria(QuestionEntity.class);
+	criteria.createAlias("interview", "i");
+	criteria.add(eq("i.user", user));
+	return criteria.list();
     }
-    
+
     @SuppressWarnings("unchecked")
     @Transactional
-    public List<QuestionEntity> getQuestionsForInterview(InterviewEntity interview) {
-        Session session = sessionFactory.getCurrentSession();
-        Criteria criteria = session.createCriteria(QuestionEntity.class);
-        criteria.add(eq("interview", interview));
-        return criteria.list();
+    public List<QuestionEntity> getQuestionsForInterview(
+	    InterviewEntity interview) {
+	Session session = sessionFactory.getCurrentSession();
+	Criteria criteria = session.createCriteria(QuestionEntity.class);
+	criteria.add(eq("interview", interview));
+	return criteria.list();
     }
-    
-    
-//    @SuppressWarnings("unchecked")
-//    @Transactional
-//    public List<InterviewEntity> getInterviewsByUser(UserEntity user) {
-//	Session session = sessionFactory.getCurrentSession();
-//	Criteria criteria = session.createCriteria(InterviewEntity.class);
-//	criteria.add(eq("user", user));
-//	return criteria.list();
-//    }
-    
-//    getQuestionsForInterview(InterviewEntity interview) 
-//    getQuestionsForUser(UserEntity user)
-
 }
