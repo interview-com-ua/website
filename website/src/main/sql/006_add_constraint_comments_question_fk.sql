9cc5c636660edc6ba1877a939a2aa1a67f11a136
MaximPedich <maximpedich@gmail.com>
2013-01-24 00:10:12 +0200

    Remove duplicated directory

--- website/src/main/sql/006_add_constraint_comments_question_fk.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE comments ADD CONSTRAINT comments_question_fk FOREIGN KEY (question_id) REFERENCES interview_question (id);
--- website/src/main/webapp/WEB-INF/views/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-	<h1>Default page it-interview</h1>	
-</body>
-</html>
