f6a6c51b9d868273ddfad604f2c8cafc52e1daa7
DmitryButolin <djemen@gmail.com>
2013-01-23 14:48:55 +0200

    Redid_FeedbackTests

--- src/test/java/ua/com/itinterview/dao/FeedbackDaoIntegrationTest.java
+++ src/test/java/ua/com/itinterview/dao/FeedbackDaoIntegrationTest.java
@@ -9,15 +9,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import com.github.stokito.gag.annotation.remark.Facepalm;
-import com.github.stokito.gag.annotation.remark.WTF;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ua.com.itinterview.entity.FeedbackEntity;
 
+import com.github.stokito.gag.annotation.remark.Facepalm;
+import com.github.stokito.gag.annotation.remark.WTF;
+
 public class FeedbackDaoIntegrationTest extends
-	BaseEntityWithIdDaoIntegrationTest {
+	BaseEntityWithIdDaoIntegrationTest<FeedbackEntity> {
 
     @Autowired
     private FeedbackDao feedbackDao;
@@ -38,10 +39,16 @@ public class FeedbackDaoIntegrationTest extends
     public void testGetAllUncheckedFeedbacks() {
 	DateFormat formatter;
 	Date date;
+	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	FeedbackEntity entity = createEntity();
 
+	System.out
+		.println("****************************************************");
+	List<FeedbackEntity> list2 = feedbackDao.getAllUncheckedFeedbacks();
+	System.out
+		.println("****************************************************");
+
 	String str_date = "2012-12-07";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
 	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
@@ -56,13 +63,11 @@ public class FeedbackDaoIntegrationTest extends
 
 	FeedbackEntity entity1 = createEntity();
 	str_date = "2012-12-20";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
 	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity1.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	entity1.setFeedbackText("hhhghghg22222");
@@ -71,9 +76,8 @@ public class FeedbackDaoIntegrationTest extends
 
 	FeedbackEntity entity2 = createEntity();
 	str_date = "2012-12-25";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
-	    date = (Date) formatter.parse(str_date); //WTF?
+	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity2.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
@@ -87,11 +91,10 @@ public class FeedbackDaoIntegrationTest extends
 	str_date = "2013-01-20";
 	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
-	    date = (Date) formatter.parse(str_date);   //WTF?
+	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity3.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	entity3.setFeedbackText("hhhghghg4444");
@@ -113,16 +116,26 @@ public class FeedbackDaoIntegrationTest extends
 	DateFormat formatter;
 	Date date, dateFrom, dateTo;
 
-	FeedbackEntity entity = createEntity();
-
-	String str_date = "2012-12-07";
+	String str_date = "2012-12-24";
 	formatter = new SimpleDateFormat("yyyy-MM-dd");
+	dateFrom = (Date) formatter.parse(str_date);
+
+	str_date = "2012-12-26";
+	dateTo = (Date) formatter.parse(str_date);
+
+	System.out
+		.println("****************************************************");
+	List<FeedbackEntity> list = feedbackDao.getFeedbacksForPeriod(dateFrom,
+		dateTo);
+	System.out
+		.println("****************************************************");
+	FeedbackEntity entity = createEntity();
+	str_date = "2012-12-07";
 	try {
 	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 
@@ -132,14 +145,12 @@ public class FeedbackDaoIntegrationTest extends
 
 	FeedbackEntity entity1 = createEntity();
 	str_date = "2012-12-20";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
 	    date = (Date) formatter.parse(str_date);
 
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity1.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	entity1.setFeedbackText("hhhghghg1111");
@@ -148,13 +159,11 @@ public class FeedbackDaoIntegrationTest extends
 
 	FeedbackEntity entity2 = createEntity();
 	str_date = "2012-12-25";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
 	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity2.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	entity2.setFeedbackText("hhhghghg22222");
@@ -163,34 +172,21 @@ public class FeedbackDaoIntegrationTest extends
 
 	FeedbackEntity entity3 = createEntity();
 	str_date = "2013-01-20";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
 	try {
 	    date = (Date) formatter.parse(str_date);
 	    java.sql.Timestamp timeStampDate = new Timestamp(date.getTime());
 	    entity3.setCreateTime(timeStampDate);
 	} catch (ParseException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 	entity3.setFeedbackText("hhhghghg333333");
 	entity3.setChecked(false);
 	entity3 = feedbackDao.save(entity3);
 
-	str_date = "2012-12-24";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
-	dateFrom = (Date) formatter.parse(str_date);
-
-	str_date = "2012-12-26";
-	formatter = new SimpleDateFormat("yyyy-MM-dd");
-	dateTo = (Date) formatter.parse(str_date);
-
-	System.out.println(dateFrom.toString());
-	System.out.println(dateTo.toString());
-
-	List<FeedbackEntity> list = feedbackDao.getFeedbacksForPeriod(dateFrom,
-		dateTo);
-	assertEquals(1, list.size());
-	assertEquals(entity2, list.get(0));
+	List<FeedbackEntity> list2 = feedbackDao.getFeedbacksForPeriod(
+		dateFrom, dateTo);
+	assertEquals(1, list2.size());
+	assertEquals(entity2, list2.get(0));
 
     }
 
