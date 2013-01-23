384cdf9ece2b6ca761657f6f0b24501a0deab411
MaximPedich <maximpedich@gmail.com>
2013-01-24 00:10:12 +0200

    Remove duplicated directory

--- website/src/main/sql/003_create_feedbacks.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE feedbacks (
-  id           INTEGER NOT NULL,
-  createTime   TIMESTAMP,
-  checked      BOOLEAN,
-  feedbackText TEXT,
-  PRIMARY KEY (id)
-);
-
-CREATE SEQUENCE feedbacks_id START WITH 1;
