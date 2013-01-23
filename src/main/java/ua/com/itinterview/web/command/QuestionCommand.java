53e3fb9b17ae98fc59be8f46222f04de79f921f5
Viktor Kuchyn <kuchin.victor@gmail.com>
2013-01-23 20:39:51 +0200

    Issue#47: added jsp page for question list. Added controller method to show questions. Got problems with elin forEach, postponed it for future

---
+++ src/main/java/ua/com/itinterview/web/command/QuestionCommand.java
@@ -0,0 +1,46 @@
+package ua.com.itinterview.web.command;
+
+public class QuestionCommand {
+
+    private String question;
+
+    public String getQuestion() {
+	return question;
+    }
+
+    public void setQuestion(String question) {
+	this.question = question;
+    }
+
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result
+		+ ((question == null) ? 0 : question.hashCode());
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	QuestionCommand other = (QuestionCommand) obj;
+	if (question == null) {
+	    if (other.question != null)
+		return false;
+	} else if (!question.equals(other.question))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "QuestionCommand [question=" + question + "]";
+    }
+
+}
--- src/main/java/ua/com/itinterview/web/resource/InterviewResource.java
+++ src/main/java/ua/com/itinterview/web/resource/InterviewResource.java
@@ -1,10 +1,26 @@
 package ua.com.itinterview.web.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import ua.com.itinterview.web.command.QuestionCommand;
 
 @Controller
-@RequestMapping(value = "interview")
+@RequestMapping(value = "/interview")
 public class InterviewResource {
 
+    @RequestMapping(value = "/{interviewId}/question_list", method = RequestMethod.GET)
+    public ModelAndView showQuestionListFoeInterview(
+	    @PathVariable Integer interviewId) {
+	List<QuestionCommand> questionList = new ArrayList<QuestionCommand>();
+	ModelAndView view = new ModelAndView("show_question_list");
+	view.addObject(questionList);
+	return view;
+    }
 }
