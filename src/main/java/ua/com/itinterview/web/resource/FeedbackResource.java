d985c2e7f1a35556313bdc410196dc1736b63c29
voch83 <voch83@gmail.com>
2013-01-28 17:20:59 +0200

    added signup.jsp and fedbackResource.java

--- src/main/java/ua/com/itinterview/web/resource/FeedbackResource.java
+++ src/main/java/ua/com/itinterview/web/resource/FeedbackResource.java
@@ -2,9 +2,15 @@ package ua.com.itinterview.web.resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/feedback")
 public class FeedbackResource {
-
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView getAddFeedbackPage() {
+	ModelAndView view = new ModelAndView("add_feedback");
+	return view;
+    }
 }
