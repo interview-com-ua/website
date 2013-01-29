89689542350305da3db90339bd7d2b4d85cf6a02
MaximPedich <maximpedich@gmail.com>
2013-01-30 00:31:05 +0200

    #44 issues step - 1,2,3

--- src/main/java/ua/com/itinterview/web/resource/FeedbackResource.java
+++ src/main/java/ua/com/itinterview/web/resource/FeedbackResource.java
@@ -1,9 +1,14 @@
 package ua.com.itinterview.web.resource;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import ua.com.itinterview.web.command.CommentCommand;
 
 @Controller
 @RequestMapping(value = "/feedback")
@@ -12,6 +17,12 @@ public class FeedbackResource {
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public ModelAndView getAddFeedbackPage() {
 	ModelAndView view = new ModelAndView("add_feedback");
+	return view;
+    }
+    
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ModelAndView showFeedbackList() {
+	ModelAndView view = new ModelAndView("show_feedback_list");
 	return view;
     }
 }
