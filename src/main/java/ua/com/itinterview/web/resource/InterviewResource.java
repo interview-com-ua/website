cd77f3348a3bcc750ee08a065bad74ea09cd6620
MaximPedich <maximpedich@gmail.com>
2013-01-30 20:02:58 +0200

    add import java.util.List in InterviewResource.java

--- src/main/java/ua/com/itinterview/web/resource/InterviewResource.java
+++ src/main/java/ua/com/itinterview/web/resource/InterviewResource.java
@@ -1,6 +1,7 @@
 package ua.com.itinterview.web.resource;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
