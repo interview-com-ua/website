226e1da680e133281a93b500f58818a5a3723630
MaximPedich <maximpedich@gmail.com>
2013-01-30 15:09:29 +0200

    Add BootStrap.java and Enviroment.java. Modify context.xml

---
+++ src/main/java/ua/com/itinterview/web/configuration/BootStrap.java
@@ -0,0 +1,30 @@
+package ua.com.itinterview.web.configuration;
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ua.com.itinterview.dao.FeedbackDao;
+import ua.com.itinterview.entity.FeedbackEntity;
+
+public class BootStrap {
+
+    private String environment;
+
+    @Autowired
+    FeedbackDao feedbackDao;
+
+    public void init() {
+    }
+
+    public void destroy() {
+
+    }
+
+    public String getEnvironment() {
+	return environment;
+    }
+
+    public void setEnvironment(String environment) {
+	this.environment = environment;
+    }
+}
