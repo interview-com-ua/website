bb0de57885ee429eba1e9120e1b4aa3bfd180a87
MaximPedich <maximpedich@gmail.com>
2013-01-30 02:20:58 +0200

    #44. add showFeedbackList() in FeedbackResource.java

--- src/main/java/ua/com/itinterview/web/resource/FeedbackResource.java
+++ src/main/java/ua/com/itinterview/web/resource/FeedbackResource.java
@@ -2,17 +2,21 @@ package ua.com.itinterview.web.resource;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import ua.com.itinterview.web.command.CommentCommand;
+import ua.com.itinterview.service.FeedbackService;
+import ua.com.itinterview.web.command.FeedbackCommand;
 
 @Controller
 @RequestMapping(value = "/feedback")
 public class FeedbackResource {
+    
+    @Autowired
+    private FeedbackService feedbackService;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public ModelAndView getAddFeedbackPage() {
@@ -22,7 +26,9 @@ public class FeedbackResource {
     
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public ModelAndView showFeedbackList() {
+	List<FeedbackCommand> feedbackList = feedbackService.getFeedbackList();
 	ModelAndView view = new ModelAndView("show_feedback_list");
+	view.addObject("feedbackList", feedbackList);
 	return view;
     }
 }
