fe1db988aad9f723af237a5ebcbabfd1d55d69f0
Vladimir <vladprut@bigmir.net>
2013-01-30 19:15:42 +0200

    Issues 37 and 37 show_Interview_list

--- src/main/java/ua/com/itinterview/service/InterviewService.java
+++ src/main/java/ua/com/itinterview/service/InterviewService.java
@@ -1,8 +1,16 @@
 package ua.com.itinterview.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import ua.com.itinterview.dao.InterviewEntityDao;
+import ua.com.itinterview.dao.UserDao;
 import ua.com.itinterview.entity.InterviewEntity;
 import ua.com.itinterview.web.command.InterviewCommand;
 
@@ -10,8 +18,32 @@ public class InterviewService {
     @Autowired
     InterviewEntityDao interviewEntityDao;
 
+    @Autowired
+    UserDao userDao;
+
+    @Autowired
+    protected SessionFactory sessionFactory;
+
     public void addInterview(InterviewCommand inputInterview) {
 	InterviewEntity interviewEntity = new InterviewEntity(inputInterview);
 	interviewEntityDao.save(interviewEntity);
+    }
+
+    @Transactional
+    public List<InterviewCommand> getInterviewList() {
+	Session session = sessionFactory.getCurrentSession();
+	Criteria criteria = session.createCriteria(InterviewEntity.class);
+	@SuppressWarnings("unchecked")
+	List<InterviewEntity> list = criteria.list();
+	return convertToInterviewCommandList(list);
+    }
+
+    private List<InterviewCommand> convertToInterviewCommandList(
+	    List<InterviewEntity> interviewEntities) {
+	List<InterviewCommand> result = new ArrayList<InterviewCommand>();
+	for (InterviewEntity interviewEntity : interviewEntities) {
+	    result.add(new InterviewCommand(interviewEntity));
+	}
+	return result;
     }
 }
