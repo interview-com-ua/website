4fad7c3c16adb2235f5043cf5ddae101da3bde71
Andrey Stasyuk <shmishleniy@gmail.com>
2013-02-10 18:26:06 +0200

    Issue #81: Bug-fix: Added error message whith error key in FieldsEqualsValidator when constraint is violated.

--- src/main/java/ua/com/itinterview/validation/FieldsEqualsValidator.java
+++ src/main/java/ua/com/itinterview/validation/FieldsEqualsValidator.java
@@ -6,15 +6,25 @@ import javax.validation.ConstraintValidatorContext;
 public class FieldsEqualsValidator implements
 	ConstraintValidator<FieldsEquals, Object> {
 
+    private String errorKey;
+    private String errorMessage;
+
     @Override
     public void initialize(FieldsEquals constraintAnnotations) {
-	// TODO Auto-generated method stub
-
+	errorKey = constraintAnnotations.errorKey();
+	errorMessage = constraintAnnotations.message();
     }
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
+	Boolean valid = false;
+
+	if (!valid) {
+	    context.disableDefaultConstraintViolation();
+	    context.buildConstraintViolationWithTemplate(errorMessage)
+		    .addNode(errorKey).addConstraintViolation();
+	}
+	
 	return false;
     }
-
 }
