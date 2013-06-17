e8d7846739f9800fcfc034b8366cefa49f0ce3a4
Andrey Stasyuk <shmishleniy@gmail.com>
2013-06-17 08:49:04 +0300

    Added FilterChain to test spring security. Added to BaseWebIntegrationTest login, logout methods. Added methods to work with cookie and methods to create user in database.

--- src/test/java/ua/com/itinterview/web/integration/BaseWebIntegrationTest.java
+++ src/test/java/ua/com/itinterview/web/integration/BaseWebIntegrationTest.java
@@ -1,28 +1,38 @@
 package ua.com.itinterview.web.integration;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
+import ua.com.itinterview.dao.UserDao;
+import ua.com.itinterview.entity.UserEntity;
 import ua.com.itinterview.web.security.AuthenticationUtils;
+
+import javax.servlet.http.Cookie;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "classpath:test-web-context.xml" })
+@ContextConfiguration({"classpath:test-web-context.xml"})
 public abstract class BaseWebIntegrationTest extends
-	AbstractTransactionalJUnit4SpringContextTests {
+        AbstractTransactionalJUnit4SpringContextTests {
 
     protected MockMvc mvc;
 
@@ -30,29 +40,85 @@ public abstract class BaseWebIntegrationTest extends
     private WebApplicationContext webApplicationContext;
 
     @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    @Autowired
+    UserDao userDao;
+
+    @Autowired
     private AuthenticationUtils authenticationUtils;
 
     @Before
     public void setUp() {
-	mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(springSecurityFilterChain)
+                .build();
     }
 
     protected MockHttpServletRequestBuilder registerUser(String userName,
-	    String name, String email, String password, String confirmPassword) {
-	return post("/register").param("userName", userName)
-		.param("name", name).param("email", email)
-		.param("password", password)
-		.param("confirmPassword", confirmPassword);
+                                                         String name, String email, String password, String confirmPassword) {
+        return post("/register").param("userName", userName)
+                .param("name", name).param("email", email)
+                .param("password", password)
+                .param("confirmPassword", confirmPassword);
     }
 
     protected MockHttpServletRequestBuilder loginUser(String userName,
-	    String password) {
-	return post("/j_spring_security_check").param("j_username", userName)
-		.param("j_password", password);
+                                                      String password) {
+        return post("/j_spring_security_check")
+                .param("j_username", userName)
+                .param("j_password", password)
+                .param("_spring_security_remember_me", "on");
     }
 
     @After
     public void logout() {
-	SecurityContextHolder.getContext().setAuthentication(null);
+        SecurityContextHolder.getContext().setAuthentication(null);
+    }
+
+    public MockHttpServletRequestBuilder logout(MockHttpSession session) throws Exception {
+        return (post("/j_spring_security_logout").session(session));
+    }
+
+    @Rollback
+    public UserEntity createUser(String userName, String name, String email, String password) throws Exception {
+        return createUser(userName, name, email, password, UserEntity.Sex.FEMALE);
+    }
+
+    @Rollback
+    public UserEntity createUser(String userName, String name, String email, String password, UserEntity.Sex sex) throws Exception {
+        UserEntity userToSave = new UserEntity();
+        userToSave.setUserName(userName);
+        userToSave.setName(name);
+        userToSave.setEmail(email);
+        userToSave.setPassword(authenticationUtils.getMD5Hash(password));
+        userToSave.setSex(sex);
+        return userDao.save(userToSave);
+    }
+
+    public static Cookie[] initCookies() {
+        return new Cookie[]{
+                new Cookie("MY-COOKIE", "DEFAULT_COOKIE_VALUE")
+        };
+    }
+
+    public static Cookie[] updateCookies(Cookie[] current,
+                                         ResultActions result) {
+        Map<String, Cookie> currentCookies = new HashMap<String, Cookie>();
+        if (current != null) {
+            for (Cookie c : current) {
+                currentCookies.put(c.getName(), c);
+            }
+        }
+
+        Cookie[] newCookies = result.andReturn().getResponse().getCookies();
+        for (Cookie newCookie : newCookies) {
+            if (StringUtils.isBlank(newCookie.getValue())) {
+                currentCookies.remove(newCookie.getName());
+            } else {
+                currentCookies.put(newCookie.getName(), newCookie);
+            }
+        }
+        return currentCookies.values().toArray(new Cookie[currentCookies.size()]);
     }
 }
--- src/test/java/ua/com/itinterview/web/security/SpringSecurityTest.java
+++ src/test/java/ua/com/itinterview/web/security/SpringSecurityTest.java
@@ -1,11 +1,25 @@
 package ua.com.itinterview.web.security;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Andrey
- * Date: 6/13/13
- * Time: 10:23 PM
- * To change this template use File | Settings | File Templates.
- */
-public class SpringSecurityTest {
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import ua.com.itinterview.dao.UserDao;
+import ua.com.itinterview.entity.UserEntity;
+import ua.com.itinterview.web.integration.BaseWebIntegrationTest;
+
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class SpringSecurityTest extends BaseWebIntegrationTest {
+
 }
