ba4f48c4dc978fa2f68d255865e15b8983079c72
Andrey Stasyuk <shmishleniy@gmail.com>
2013-06-26 23:11:28 +0300

    Ignore SpringCesurityTest

--- src/test/java/ua/com/itinterview/web/security/SpringSecurityTest.java
+++ src/test/java/ua/com/itinterview/web/security/SpringSecurityTest.java
@@ -1,25 +1,10 @@
 package ua.com.itinterview.web.security;
 
 
-import junit.framework.Assert;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import ua.com.itinterview.dao.UserDao;
-import ua.com.itinterview.entity.UserEntity;
+import org.junit.Ignore;
 import ua.com.itinterview.web.integration.BaseWebIntegrationTest;
 
-import javax.servlet.http.HttpSession;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+@Ignore
 public class SpringSecurityTest extends BaseWebIntegrationTest {
 
 }
