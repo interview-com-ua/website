a7d33f05c01a84bbe62442c041812d2589f63d3d
Andrey Stasyuk <shmishleniy@gmail.com>
2013-09-13 13:11:15 +0300

    Refactoring of the QuestionDaoIntegrationTest

--- src/test/java/ua/com/itinterview/dao/QuestionDaoIntegrationTest.java
+++ src/test/java/ua/com/itinterview/dao/QuestionDaoIntegrationTest.java
@@ -1,18 +1,27 @@
 package ua.com.itinterview.dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
 
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ua.com.itinterview.entity.InterviewEntity;
 import ua.com.itinterview.entity.QuestionEntity;
 
+import javax.persistence.EntityNotFoundException;
+
 public class QuestionDaoIntegrationTest extends
-	BaseEntityWithIdDaoIntegrationTest<QuestionEntity> {
+        BaseEntityWithIdDaoIntegrationTest<QuestionEntity> {
+
+    public static final String INTERVIEW_FEEDBACK = "Interwview feedback";
+    public static final String QUESTION_TEXT = "Question text";
+    public static final String QUESTION_TITLE = "Question title";
 
     @Autowired
     private InterviewDao interDao;
@@ -20,65 +29,77 @@ public class QuestionDaoIntegrationTest extends
     @Autowired
     private QuestionDao questionDao;
 
-    @Autowired
-    private UserDao userDao;
-
     @Override
     protected QuestionEntity createEntity() {
-	InterviewEntity interview = createInterview();
-	QuestionEntity question = new QuestionEntity();
-	question.setQuestion("What is DAO?");
-	question.setCreate(new Date());
-	question.setInterview(interview);
-	return question;
-    }
-
-    private InterviewEntity createInterview() {
-	InterviewEntity interview = new InterviewEntity();
-	interview.setCreated(new Date());
-	interview.setFeedback("Tra-ta-ta");
-	interview.setUser(testUser);
-	interview = interDao.save(interview);
-	return interview;
+        InterviewEntity interview = createInterview();
+        return createQuestion(interview);
     }
 
     @Override
     protected EntityWithIdDao<QuestionEntity> getEntityWithIdDao() {
-	return questionDao;
+        return questionDao;
+    }
+
+    private InterviewEntity createInterview() {
+        InterviewEntity interview = new InterviewEntity();
+        interview.setCreated(new Date());
+        interview.setFeedback(INTERVIEW_FEEDBACK);
+        interview.setUser(testUser);
+        return interDao.save(interview);
+    }
+
+    private QuestionEntity createQuestion() {
+        return createQuestion(null);
+    }
+
+    private QuestionEntity createQuestion(InterviewEntity interviewEntity)
+    {
+        QuestionEntity questionEntity = new QuestionEntity();
+        questionEntity.setQuestion(QUESTION_TEXT);
+        questionEntity.setTitle(QUESTION_TITLE);
+        questionEntity.setInterview(interviewEntity);
+        return questionDao.save(questionEntity);
+    }
+
+    @Test
+    public void testGetQuestionForInterviewThatDoesNotExist()
+    {
+        InterviewEntity wrongInterview = new InterviewEntity();
+        wrongInterview.setId(2143);
+        List<QuestionEntity> questionsFound = questionDao.getQuestionsForInterview(wrongInterview);
+        assertEquals(0, questionsFound.size());
     }
 
     @Test
     public void testGetQuestionsForInterview() {
-	InterviewEntity interview1 = createInterview();
-	InterviewEntity interview = createInterview();
-	QuestionEntity question1 = createQuestionAndInsertToDB(interview1);
-	createQuestionAndInsertToDB(interview);
-	// When
-	List<QuestionEntity> list = questionDao
-		.getQuestionsForInterview(interview1);
-        questionDao.getQuestionsCountForInterview(interview1);
-	// Then
-	assertEquals(1, list.size());
-	assertEquals(question1.getQuestion(), list.get(0).getQuestion());
+        InterviewEntity interview = createInterview();
+        QuestionEntity expectedQuestion = createQuestion(interview);
+        InterviewEntity anotherInterview = createInterview();
+        QuestionEntity unexpectedQuestion = createQuestion(anotherInterview);
+        List<QuestionEntity> actualQuestions = questionDao.getQuestionsForInterview(interview);
+        assertEquals(1, actualQuestions.size());
+        assertEquals(expectedQuestion, actualQuestions.get(0));
+        assertNotSame(unexpectedQuestion, actualQuestions.get(0));
+    }
+
+    @Test
+    public void testGetQuestionsCountForInterview() {
+        InterviewEntity interview = createInterview();
+        createQuestion(interview);
+        createQuestion(interview);
+        createQuestion(interview);
+        InterviewEntity anotherInterview = createInterview();
+        createQuestion(anotherInterview);
+        long questionsCountForInterview = questionDao.getQuestionsCountForInterview(interview);
+        assertEquals(3L, questionsCountForInterview);
     }
 
     @Test
     public void getQuestionsForUser() {
-	InterviewEntity interview = createInterview();
-	QuestionEntity question = createQuestionAndInsertToDB(interview);
-	// When
-	List<QuestionEntity> list = questionDao.getQuestionsForUser(testUser);
-	// Then
-	assertEquals(1, list.size());
-	assertEquals(question, list.get(0));
+        InterviewEntity interview = createInterview();
+        QuestionEntity expectedQuestion = createQuestion(interview);
+        List<QuestionEntity> actualQuestions = questionDao.getQuestionsForUser(testUser);
+        assertEquals(1, actualQuestions.size());
+        assertEquals(expectedQuestion, actualQuestions.get(0));
     }
-
-    private QuestionEntity createQuestionAndInsertToDB(InterviewEntity interview) {
-	QuestionEntity question = new QuestionEntity();
-	question.setInterview(interview);
-	question.setCreate(new Date());
-	question.setQuestion("dkjdkf");
-	return questionDao.save(question);
-    }
-
 }
