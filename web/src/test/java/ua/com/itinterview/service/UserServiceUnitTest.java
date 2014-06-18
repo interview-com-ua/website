7afcb5ba668ea0534bb476aaec4c330ad9fb236d
Viktor Kuchyn <kuchin.victor@gmail.com>
2014-06-18 19:13:37 +0300

    #76: reformatted code

--- web/src/test/java/ua/com/itinterview/service/UserServiceUnitTest.java
+++ web/src/test/java/ua/com/itinterview/service/UserServiceUnitTest.java
@@ -12,14 +12,11 @@ import ua.com.itinterview.web.command.UserEditProfileCommand;
 
 import javax.persistence.EntityNotFoundException;
 
-import static org.easymock.EasyMock.capture;
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
+import static org.easymock.EasyMock.*;
 import static org.junit.Assert.assertEquals;
 
-public class UserServiceUnitTest {
+public class UserServiceUnitTest
+{
 
     private static final int USER_ID = 1;
     private static final String EMAIL = "user@email.com";
@@ -36,153 +33,166 @@ public class UserServiceUnitTest {
     private PasswordEncoder passwordEncoder;
 
     @Before
-    public void createMocks() {
-	userDaoMock = createMock(UserDao.class);
-	userService = new UserService();
-	userService.userDao = userDaoMock;
-	passwordEncoder = createMock(PasswordEncoder.class);
-	userService.passwordEncoder = passwordEncoder;
+    public void createMocks()
+    {
+        userDaoMock = createMock(UserDao.class);
+        userService = new UserService();
+        userService.userDao = userDaoMock;
+        passwordEncoder = createMock(PasswordEncoder.class);
+        userService.passwordEncoder = passwordEncoder;
     }
 
-    private void replayAllMocks() {
-	replay(userDaoMock, passwordEncoder);
-    }
-
-    @Test
-    public void testConvertUserCommandToUserEntity() {
-	replayAllMocks();
-	UserCommand userCommand = createUserCommandWithPassword();
-	UserEntity expectedUserEntity = createUserEntity();
-	UserEntity actualUserEntity = new UserEntity(userCommand);
-	assertEquals(expectedUserEntity, actualUserEntity);
+    private void replayAllMocks()
+    {
+        replay(userDaoMock, passwordEncoder);
     }
 
     @Test
-    public void testConvertUserEntityToUserCommand() {
-	replayAllMocks();
-	UserEntity userEntity = createUserEntity();
-	UserCommand expectedUserCommand = createUserCommand();
-	UserCommand actualUserCommand = new UserCommand(userEntity);
-	assertEquals(expectedUserCommand, actualUserCommand);
+    public void testConvertUserCommandToUserEntity()
+    {
+        replayAllMocks();
+        UserCommand userCommand = createUserCommandWithPassword();
+        UserEntity expectedUserEntity = createUserEntity();
+        UserEntity actualUserEntity = new UserEntity(userCommand);
+        assertEquals(expectedUserEntity, actualUserEntity);
     }
 
     @Test
-    public void testConvertUserEntityToUserEditProfileCommand() {
-	replayAllMocks();
-	UserEntity userEntity = createUserEntity();
-	UserEditProfileCommand actualUserEditProfileCommand = new UserEditProfileCommand(
-		userEntity);
-
-	UserEditProfileCommand expectedUserEditProfileCommand = creatUserEditProfileCommand();
-	assertEquals(expectedUserEditProfileCommand,
-		actualUserEditProfileCommand);
+    public void testConvertUserEntityToUserCommand()
+    {
+        replayAllMocks();
+        UserEntity userEntity = createUserEntity();
+        UserCommand expectedUserCommand = createUserCommand();
+        UserCommand actualUserCommand = new UserCommand(userEntity);
+        assertEquals(expectedUserCommand, actualUserCommand);
     }
 
     @Test
-    public void testConvertUserCommandToUserEditProfileCommand() {
-	replayAllMocks();
-	UserCommand userCommand = createUserCommand();
-	UserEditProfileCommand actualUserEditProfileCommand = new UserEditProfileCommand(
-		userCommand);
+    public void testConvertUserEntityToUserEditProfileCommand()
+    {
+        replayAllMocks();
+        UserEntity userEntity = createUserEntity();
+        UserEditProfileCommand actualUserEditProfileCommand = new UserEditProfileCommand(
+                userEntity);
 
-	UserEditProfileCommand expectedUserEditProfileCommand1 = creatUserEditProfileCommand();
-	assertEquals(expectedUserEditProfileCommand1,
-		actualUserEditProfileCommand);
+        UserEditProfileCommand expectedUserEditProfileCommand = creatUserEditProfileCommand();
+        assertEquals(expectedUserEditProfileCommand,
+                actualUserEditProfileCommand);
     }
 
     @Test
-    public void testCreateUser() {
-	UserCommand userCommand = createUserCommandWithPassword();
-	UserEntity userEntity = new UserEntity(userCommand);
-	userEntity.setPassword(ENCODED_PASSWORD);
+    public void testConvertUserCommandToUserEditProfileCommand()
+    {
+        replayAllMocks();
+        UserCommand userCommand = createUserCommand();
+        UserEditProfileCommand actualUserEditProfileCommand = new UserEditProfileCommand(
+                userCommand);
 
-	expect(userDaoMock.save(userEntity)).andReturn(userEntity);
-	expect(passwordEncoder.encodePassword(PASSWORD, "")).andReturn(
-		ENCODED_PASSWORD);
-	replayAllMocks();
-
-	UserCommand actualUserCommand = userService.createUser(userCommand);
-	UserCommand expectedUserCommand = createUserCommand();
-	expectedUserCommand.setId(actualUserCommand.getId());
-	assertEquals(expectedUserCommand, actualUserCommand);
+        UserEditProfileCommand expectedUserEditProfileCommand1 = creatUserEditProfileCommand();
+        assertEquals(expectedUserEditProfileCommand1,
+                actualUserEditProfileCommand);
     }
 
     @Test
-    public void testUpdateUser() {
-	UserEntity oldUserInDb = createUserEntity();
-	UserCommand userToUpdate = createCustomUserCommand(NEW_USER_ID, PASSWORD, NEW_EMAIL, NEW_NAME);
+    public void testCreateUser()
+    {
+        UserCommand userCommand = createUserCommandWithPassword();
+        UserEntity userEntity = new UserEntity(userCommand);
+        userEntity.setPassword(ENCODED_PASSWORD);
 
-	expect(userDaoMock.getEntityById(USER_ID)).andReturn(oldUserInDb);
-	Capture<UserEntity> userToSaveCapture = new Capture<UserEntity>();
-	expect(userDaoMock.save(capture(userToSaveCapture))).andReturn(
-		oldUserInDb);
-	replayAllMocks();
+        expect(userDaoMock.save(userEntity)).andReturn(userEntity);
+        expect(passwordEncoder.encodePassword(PASSWORD, "")).andReturn(
+                ENCODED_PASSWORD);
+        replayAllMocks();
 
-	UserCommand expectedCommand = createCustomUserCommand(USER_ID, null, NEW_EMAIL, NEW_NAME);
-	assertEquals(expectedCommand,
-		userService.updateUser(USER_ID, userToUpdate));
-
-	UserEntity actualSavedEntity = userToSaveCapture.getValue();
-	assertEquals(NEW_EMAIL, actualSavedEntity.getEmail());
-	assertEquals(NEW_NAME, actualSavedEntity.getName());
-	assertEquals(USER_ID, actualSavedEntity.getId());
-	assertEquals(PASSWORD, actualSavedEntity.getPassword());
+        UserCommand actualUserCommand = userService.createUser(userCommand);
+        UserCommand expectedUserCommand = createUserCommand();
+        expectedUserCommand.setId(actualUserCommand.getId());
+        assertEquals(expectedUserCommand, actualUserCommand);
     }
 
     @Test
-    public void testUpdateUserEditProfile() {
-	UserEntity userEntityToChange = createUserEntity();
-	expect(userDaoMock.getEntityById(USER_ID))
-		.andReturn(userEntityToChange);
-	Capture<UserEntity> savedUserEntityCapture = new Capture<UserEntity>();
-	expect(userDaoMock.save(capture(savedUserEntityCapture))).andReturn(
-		userEntityToChange);
-	replayAllMocks();
+    public void testUpdateUser()
+    {
+        UserEntity oldUserInDb = createUserEntity();
+        UserCommand userToUpdate = createCustomUserCommand(NEW_USER_ID, PASSWORD, NEW_EMAIL, NEW_NAME);
 
-	UserEditProfileCommand changedUserProfile = createCustomUserEditProfileCommand(NEW_USER_ID, NEW_NAME, NEW_EMAIL);
-	UserEditProfileCommand actualUserEditProfile = userService.updateUser(
-		USER_ID, changedUserProfile);
+        expect(userDaoMock.getEntityById(USER_ID)).andReturn(oldUserInDb);
+        Capture<UserEntity> userToSaveCapture = new Capture<UserEntity>();
+        expect(userDaoMock.save(capture(userToSaveCapture))).andReturn(
+                oldUserInDb);
+        replayAllMocks();
 
-	UserEntity savedUserEntity = savedUserEntityCapture.getValue();
-	assertEquals(USER_ID, savedUserEntity.getId());
-	assertEquals(PASSWORD, savedUserEntity.getPassword());
-	assertEquals(NEW_NAME, savedUserEntity.getName());
-	assertEquals(NEW_EMAIL, savedUserEntity.getEmail());
+        UserCommand expectedCommand = createCustomUserCommand(USER_ID, null, NEW_EMAIL, NEW_NAME);
+        assertEquals(expectedCommand,
+                userService.updateUser(USER_ID, userToUpdate));
 
-	UserEditProfileCommand expectedUserProfile = createCustomUserEditProfileCommand(USER_ID, NEW_NAME, NEW_EMAIL);
-	assertEquals(expectedUserProfile, actualUserEditProfile);
+        UserEntity actualSavedEntity = userToSaveCapture.getValue();
+        assertEquals(NEW_EMAIL, actualSavedEntity.getEmail());
+        assertEquals(NEW_NAME, actualSavedEntity.getName());
+        assertEquals(USER_ID, actualSavedEntity.getId());
+        assertEquals(PASSWORD, actualSavedEntity.getPassword());
+    }
+
+    @Test
+    public void testUpdateUserEditProfile()
+    {
+        UserEntity userEntityToChange = createUserEntity();
+        expect(userDaoMock.getEntityById(USER_ID))
+                .andReturn(userEntityToChange);
+        Capture<UserEntity> savedUserEntityCapture = new Capture<UserEntity>();
+        expect(userDaoMock.save(capture(savedUserEntityCapture))).andReturn(
+                userEntityToChange);
+        replayAllMocks();
+
+        UserEditProfileCommand changedUserProfile = createCustomUserEditProfileCommand(NEW_USER_ID, NEW_NAME, NEW_EMAIL);
+        UserEditProfileCommand actualUserEditProfile = userService.updateUser(
+                USER_ID, changedUserProfile);
+
+        UserEntity savedUserEntity = savedUserEntityCapture.getValue();
+        assertEquals(USER_ID, savedUserEntity.getId());
+        assertEquals(PASSWORD, savedUserEntity.getPassword());
+        assertEquals(NEW_NAME, savedUserEntity.getName());
+        assertEquals(NEW_EMAIL, savedUserEntity.getEmail());
+
+        UserEditProfileCommand expectedUserProfile = createCustomUserEditProfileCommand(USER_ID, NEW_NAME, NEW_EMAIL);
+        assertEquals(expectedUserProfile, actualUserEditProfile);
     }
 
     @Test(expected = EntityNotFoundException.class)
-    public void testUserUpdateWhenUserDoesNotExist() {
-	expect(userDaoMock.getEntityById(NEW_USER_ID)).andThrow(
-		new EntityNotFoundException());
-	replayAllMocks();
-	userService.updateUser(NEW_USER_ID, createUserCommand());
+    public void testUserUpdateWhenUserDoesNotExist()
+    {
+        expect(userDaoMock.getEntityById(NEW_USER_ID)).andThrow(
+                new EntityNotFoundException());
+        replayAllMocks();
+        userService.updateUser(NEW_USER_ID, createUserCommand());
     }
 
     @Test
-    public void testGetUserById() {
-	UserEntity userEntity = createUserEntity();
-	expect(userDaoMock.getEntityById(USER_ID)).andReturn(userEntity);
-	replayAllMocks();
+    public void testGetUserById()
+    {
+        UserEntity userEntity = createUserEntity();
+        expect(userDaoMock.getEntityById(USER_ID)).andReturn(userEntity);
+        replayAllMocks();
 
-	UserCommand actualUserCommand = userService.getUserById(USER_ID);
-	UserCommand expectedUserCommand = createUserCommand();
-	assertEquals(expectedUserCommand, actualUserCommand);
+        UserCommand actualUserCommand = userService.getUserById(USER_ID);
+        UserCommand expectedUserCommand = createUserCommand();
+        assertEquals(expectedUserCommand, actualUserCommand);
     }
 
     @Test(expected = EntityNotFoundException.class)
-    public void testGetUserByIdWhenUserDoesNotExist() {
-	expect(userDaoMock.getEntityById(NEW_USER_ID)).andThrow(
-		new EntityNotFoundException());
-	replayAllMocks();
+    public void testGetUserByIdWhenUserDoesNotExist()
+    {
+        expect(userDaoMock.getEntityById(NEW_USER_ID)).andThrow(
+                new EntityNotFoundException());
+        replayAllMocks();
 
-	userService.getUserById(NEW_USER_ID);
+        userService.getUserById(NEW_USER_ID);
     }
 
     @Test
-    public void testGetUserByUsername() {
+    public void testGetUserByUsername()
+    {
         UserEntity userEntity = createUserEntity();
         expect(userDaoMock.getUserByEmail(USER_NAME)).andReturn(userEntity);
         replayAllMocks();
@@ -192,57 +202,65 @@ public class UserServiceUnitTest {
     }
 
     @Test(expected = EntityNotFoundException.class)
-    public void testGetUserByUsernameWhenUserDoesNotExist() {
+    public void testGetUserByUsernameWhenUserDoesNotExist()
+    {
         expect(userDaoMock.getUserByEmail(FAKE_USER_NAME)).andThrow(new EntityNotFoundException());
         replayAllMocks();
         userService.getUserByEmail(FAKE_USER_NAME);
     }
 
     @After
-    public void verifyAllMocks() {
-	verify(userDaoMock, passwordEncoder);
+    public void verifyAllMocks()
+    {
+        verify(userDaoMock, passwordEncoder);
     }
 
-    private UserCommand createCustomUserCommand(int id, String password, String email, String name) {
-	UserCommand command = new UserCommand();
-	command.setId(id);
-	command.setConfirmPassword(password);
-	command.setPassword(password);
-	command.setEmail(email);
-	command.setName(name);
-	return command;
+    private UserCommand createCustomUserCommand(int id, String password, String email, String name)
+    {
+        UserCommand command = new UserCommand();
+        command.setId(id);
+        command.setConfirmPassword(password);
+        command.setPassword(password);
+        command.setEmail(email);
+        command.setName(name);
+        return command;
     }
 
-    private UserCommand createUserCommand() {
-	UserCommand command = createCustomUserCommand(USER_ID, null, EMAIL, NAME);
-	return command;
+    private UserCommand createUserCommand()
+    {
+        UserCommand command = createCustomUserCommand(USER_ID, null, EMAIL, NAME);
+        return command;
     }
 
-    private UserEditProfileCommand creatUserEditProfileCommand() {
-	return createCustomUserEditProfileCommand(USER_ID, NAME, EMAIL);
+    private UserEditProfileCommand creatUserEditProfileCommand()
+    {
+        return createCustomUserEditProfileCommand(USER_ID, NAME, EMAIL);
     }
 
-    private UserEditProfileCommand createCustomUserEditProfileCommand(int id, String name, String email) {
-	UserEditProfileCommand userEditProfileCommand = new UserEditProfileCommand();
-	userEditProfileCommand.setId(id);
-	userEditProfileCommand.setName(name);
-	userEditProfileCommand.setEmail(email);
-	return userEditProfileCommand;
+    private UserEditProfileCommand createCustomUserEditProfileCommand(int id, String name, String email)
+    {
+        UserEditProfileCommand userEditProfileCommand = new UserEditProfileCommand();
+        userEditProfileCommand.setId(id);
+        userEditProfileCommand.setName(name);
+        userEditProfileCommand.setEmail(email);
+        return userEditProfileCommand;
     }
 
-    private UserCommand createUserCommandWithPassword() {
-	UserCommand res = createUserCommand();
-	res.setPassword(PASSWORD);
-	res.setConfirmPassword(PASSWORD);
-	return res;
+    private UserCommand createUserCommandWithPassword()
+    {
+        UserCommand res = createUserCommand();
+        res.setPassword(PASSWORD);
+        res.setConfirmPassword(PASSWORD);
+        return res;
     }
 
-    private UserEntity createUserEntity() {
-	UserEntity userEntity = new UserEntity();
-	userEntity.setId(USER_ID);
-	userEntity.setPassword(PASSWORD);
-	userEntity.setEmail(EMAIL);
-	userEntity.setName(NAME);
-	return userEntity;
+    private UserEntity createUserEntity()
+    {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(USER_ID);
+        userEntity.setPassword(PASSWORD);
+        userEntity.setEmail(EMAIL);
+        userEntity.setName(NAME);
+        return userEntity;
     }
 }
